150d1cd82d166621c1171b3aea15d7f07f528323
aisha saleh <aisha.mohd.slaeh@gmail.com>
2025-05-26 22:35:52 +0300

    sort with page

--- pages/InventoryPage .ts
+++ pages/InventoryPage .ts
@@ -1,10 +1,16 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 
 export class InventoryPage {
   readonly page: Page;
+  readonly sortDropdown: Locator;
+  readonly productTitles: Locator;
+  readonly productPrices: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.sortDropdown = page.locator('[data-test="product-sort-container"]');
+    this.productTitles = page.locator(".inventory_item_name");
+    this.productPrices = page.locator(".inventory_item_price");
   }
 
   async addItemToCart(iteamId: string) {
@@ -13,5 +19,18 @@ export class InventoryPage {
 
   async removeItemFromCart(iteamId: string) {
     await this.page.locator(`[data-test="remove-${iteamId}"]`).click();
+  }
+
+  async sortBy(option: "az" |   "hilo") {
+    await this.sortDropdown.selectOption(option);
+  }
+
+  async getProductTitles(): Promise<string[]> {
+    return await this.productTitles.allTextContents();
+  }
+
+  async getProductPrices(): Promise<number[]> {
+    const rawPrices = await this.productPrices.allTextContents();
+    return rawPrices.map(p => parseFloat(p.replace("$", "")));
   }
 }
